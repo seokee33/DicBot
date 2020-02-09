169d98542925f91d3a8ba7b8170e6ff1f8bdd3cd
seokee33 <55632055+seokee33@users.noreply.github.com>
2020-02-09 13:00:28 +0900

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -20,7 +20,7 @@ async def on_message(message):
         await message.channel.send("!오늘날씨 지역입력\n!오늘날씨 띄우고 지역이름 ex) 대구 북구")
     elif message.content.startswith("!오늘날씨"):
         area = message.content[6:]
-        driver = webdriver.Chrome("C:/Chrome/cr.exe")
+        driver = webdriver.Chrome("https://github.com/seokee33/DicBot/blob/master/cr.exe")
         driver.get("https://www.naver.com")
         driver.find_element_by_css_selector("#query").send_keys(area+" 오늘날씨")
         driver.find_element_by_css_selector("#search_btn").click()
@@ -30,7 +30,7 @@ async def on_message(message):
         await message.channel.send("온도 : "+temperature+"도\n"+today)
     elif message.content.startswith("!전적검색"):
         gameID = message.content[6:]
-        driver = webdriver.Chrome("C:/Chrome/cr.exe")
+        driver = webdriver.Chrome("https://github.com/seokee33/DicBot/blob/master/cr.exe")
         driver.get("https://dak.gg")
         driver.find_element_by_css_selector("#frontPage > div.panel-left > form.search > input").send_keys(gameID)
         driver.find_element_by_css_selector("#frontPage > div > form > button > i").click()
